e9cab0759b4a750afb7038f95c0f0aecf38f4249
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2024-10-31 12:51:58 +0900

    add test item

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv 
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,8 +7,18 @@ ng () {
 
 res=0
 
+### 正常な入力 ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### 変な入力 ###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit "$res"
